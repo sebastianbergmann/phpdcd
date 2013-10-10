sebastianbergmann/phpdcd

commit 1486cd53be93f13ebc6144d00dd27caacc8f281e

Refactor.

PHPDCD/TextUI/Command.php
@@ -164,9 +164,14 @@ class PHPDCD_TextUI_Command
         }
 
         if (!empty($arguments)) {
-            $files = File_Iterator_Factory::getFilesAsArray(
-              $arguments, $suffixes, array(), $exclude
+            $result = File_Iterator_Factory::getFilesAsArray(
+              $arguments, $suffixes, array(), $exclude, TRUE
             );
+
+            $files      = $result['files'];
+            $commonPath = $result['commonPath'];
+
+            unset($result);
         } else {
             self::showHelp();
             exit(1);
@@ -182,59 +187,8 @@ class PHPDCD_TextUI_Command
         $result   = $detector->detectDeadCode($files, $recursive);
 
         $printer = new PHPDCD_TextUI_ResultPrinter;
-        $printer->printResult($result, self::getCommonPath($files));
+        $printer->printResult($result, $commonPath);
         unset($printer);
-    }
-
-    /**
-     * Returns the common path of a set of files.
-     *
-     * @param  array $files
-     * @return string
-     */
-    protected static function getCommonPath(array $files)
-    {
-        $count = count($files);
-
-        if ($count == 1) {
-            return dirname($files[0]) . DIRECTORY_SEPARATOR;
-        }
-
-        $_files = array();
-
-        for ($i = 0; $i < $count; $i++) {
-            $_files[$i] = explode(DIRECTORY_SEPARATOR, $files[$i]);
-
-            if (empty($_files[$i][0])) {
-                $_files[$i][0] = DIRECTORY_SEPARATOR;
-            }
-        }
-
-        $common = '';
-        $done   = FALSE;
-        $j      = 0;
-        $count--;
-
-        while (!$done) {
-            for ($i = 0; $i < $count; $i++) {
-                if ($_files[$i][$j] != $_files[$i+1][$j]) {
-                    $done = TRUE;
-                    break;
-                }
-            }
-
-            if (!$done) {
-                $common .= $_files[0][$j];
-
-                if ($j > 0) {
-                    $common .= DIRECTORY_SEPARATOR;
-                }
-            }
-
-            $j++;
-        }
-
-        return $common;
     }
 
     /**

commit 56637df92bc67fbd95b0bf518c82377ea04e9f3b

Fixed "Undefined variable: commonPath" issue in PHPDCD_TextUI_Command::main
$commonPath was introduced by
1486cd53b Refactor.
but badly refactored by
8d0ff7202 Replace phpunit/File_Iterator with symfony/Finder.

PHPDCD/TextUI/Command.php
@@ -181,7 +181,7 @@ class PHPDCD_TextUI_Command
         $result   = $detector->detectDeadCode($files, $recursive);
 
         $printer = new PHPDCD_TextUI_ResultPrinter;
-        $printer->printResult($result, $commonPath);
+        $printer->printResult($result);
         unset($printer);
     }
 

PHPDCD/TextUI/ResultPrinter.php
@@ -59,7 +59,7 @@ class PHPDCD_TextUI_ResultPrinter
      * @param array  $result
      * @param string $commonPath
      */
-    public function printResult(array $result, $commonPath)
+    public function printResult(array $result, $commonPath='')
     {
         foreach ($result as $name => $source) {
             printf(